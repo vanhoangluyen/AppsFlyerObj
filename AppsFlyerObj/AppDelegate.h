390a04006bceed64622c53c3239a38ca2b3bbbae
vanhoangluyen <vanhoangluyen@gmail.com>
2018-02-10 09:25:52 +0700

    Initial Commit

---
+++ AppsFlyerObj/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AppsFlyerObj
+//
+//  Created by LuyenBG on 2/10/18.
+//  Copyright © 2018 LuyenBG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AppsFlyerObj/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AppsFlyerObj
+//
+//  Created by LuyenBG on 2/10/18.
+//  Copyright © 2018 LuyenBG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
